e04ec6f272f258c039858511db0b43c3c43a9233
aipoool <comdtvise@gmail.com>
2024-05-11 00:50:53 +0530

    Update RegisteredUser.js

--- src/components/js/RegisteredUser.js
+++ src/components/js/RegisteredUser.js
@@ -132,16 +132,6 @@ const RegisteredUser = ({ isNewUser = true }) => {
 
                     <div class="elementor-widget-container">
                       <div class="elementor-button-wrapper container">
-                        {/* <button class="elementor-button elementor-button-link elementor-size-sm"  target="_blank">
-                                        <span class="elementor-button-content-wrapper">
-                                            <span class="elementor-button-icon elementor-align-icon-left">
-                                                <svg aria-hidden="true" class="e-font-icon-svg e-fab-chrome" viewBox="0 0 496 512" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M131.5 217.5L55.1 100.1c47.6-59.2 119-91.8 192-92.1 42.3-.3 85.5 10.5 124.8 33.2 43.4 25.2 76.4 61.4 97.4 103L264 133.4c-58.1-3.4-113.4 29.3-132.5 84.1zm32.9 38.5c0 46.2 37.4 83.6 83.6 83.6s83.6-37.4 83.6-83.6-37.4-83.6-83.6-83.6-83.6 37.3-83.6 83.6zm314.9-89.2L339.6 174c37.9 44.3 38.5 108.2 6.6 157.2L234.1 503.6c46.5 2.5 94.4-7.7 137.8-32.9 107.4-62 150.9-192 107.4-303.9zM133.7 303.6L40.4 120.1C14.9 159.1 0 205.9 0 256c0 124 90.8 226.7 209.5 244.9l63.7-124.8c-57.6 10.8-113.2-20.8-139.5-72.5z"></path>
-                                                </svg> 
-                                            </span>
-                                            <span class="elementor-button-text">Sign in With Google</span>
-                                        </span>
-                                    </button> */}
                         <div className="redirect-buttons">
                           {isNewUser ? (
                             <>
@@ -193,6 +183,9 @@ const RegisteredUser = ({ isNewUser = true }) => {
           data-element_type="container"
         ></div>
       </div>
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     </div>
   );
 };

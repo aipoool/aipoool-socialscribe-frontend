750244f1ef639153ded4a0d8edf770c09bf0b3fb
aipoool <comdtvise@gmail.com>
2024-05-11 18:33:56 +0530

    Update EnterOpenAIKey.js

--- src/components/js/EnterOpenAIKey.js
+++ src/components/js/EnterOpenAIKey.js
@@ -29,7 +29,7 @@ const EnterOpenAIKey = () => {
   useEffect(() => {
     fetchSessionData().then(() => {
       setIsLoading(false);
-    }, 7000);
+    }, 3000);
   }, []);
 
   // Check if userdata has OpenAIKey field and it is not null

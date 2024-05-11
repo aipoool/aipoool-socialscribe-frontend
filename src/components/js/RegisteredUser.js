0dbf924b86c2aa01c3e1c4e7a170d27ac2225fc7
aipoool <comdtvise@gmail.com>
2024-05-11 10:16:05 +0530

    feat: Adding the GIFs part 4

--- src/components/js/RegisteredUser.js
+++ src/components/js/RegisteredUser.js
@@ -1,5 +1,6 @@
 /* global chrome */
 import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
 import "../css/registeredUser.css";
 import axios from "axios";
 
@@ -68,125 +69,142 @@ const RegisteredUser = ({ isNewUser = true }) => {
   };
 
   return (
-    <div
-      class="elementor-element elementor-element-9e0036e e-flex e-con-boxed e-con e-parent e-lazyloaded snipcss-97bdC"
-      data-id="9e0036e"
-      data-element_type="container"
-      data-settings='{"background_background":"classic"}'
-    >
-      <div class="e-con-inner">
-        <div
-          class="elementor-element elementor-element-ed79abc e-con-full e-flex e-con e-child"
-          data-id="ed79abc"
-          data-element_type="container"
-        >
+    <>
+      <Helmet>
+      <script
+          src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+          integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+          integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+          integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+          crossorigin="anonymous"
+        ></script>
+      </Helmet>
+
+      <div
+        class="elementor-element elementor-element-9e0036e e-flex e-con-boxed e-con e-parent e-lazyloaded snipcss-97bdC"
+        data-id="9e0036e"
+        data-element_type="container"
+        data-settings='{"background_background":"classic"}'
+      >
+        <div class="e-con-inner">
           <div
-            class="elementor-element elementor-element-597b965 e-con-full e-flex e-con e-child"
-            data-id="597b965"
+            class="elementor-element elementor-element-ed79abc e-con-full e-flex e-con e-child"
+            data-id="ed79abc"
             data-element_type="container"
           >
             <div
-              class="elementor-element elementor-element-11d9bb4 e-con-full e-flex e-con e-child"
-              data-id="11d9bb4"
+              class="elementor-element elementor-element-597b965 e-con-full e-flex e-con e-child"
+              data-id="597b965"
               data-element_type="container"
             >
               <div
-                class="elementor-element elementor-element-1f4402a elementor-widget__width-initial elementor-widget-tablet__width-initial elementor-widget elementor-widget-heading"
-                data-id="1f4402a"
+                class="elementor-element elementor-element-11d9bb4 e-con-full e-flex e-con e-child"
+                data-id="11d9bb4"
+                data-element_type="container"
+              >
+                <div
+                  class="elementor-element elementor-element-1f4402a elementor-widget__width-initial elementor-widget-tablet__width-initial elementor-widget elementor-widget-heading"
+                  data-id="1f4402a"
+                  data-element_type="widget"
+                  data-widget_type="heading.default"
+                >
+                  <div class="elementor-widget-container">
+                    <h2 class="elementor-heading-title elementor-size-default">
+                      {heading}
+                    </h2>
+                    <p class="elementor-size-default">{para}</p>
+                    <div
+                      class="elementor-element elementor-element-9a3d259 elementor-widget__width-auto elementor-mobile-align-center elementor-widget-mobile__width-inherit elementor-align-left elementor-widget elementor-widget-button"
+                      data-id="9a3d259"
+                      data-element_type="widget"
+                      data-widget_type="button.default"
+                    >
+                      <div class="card-deck">
+                        <div class="card">
+                          <img
+                            class="card-img-top"
+                            src="https://user-images.githubusercontent.com/14011726/94132137-7d4fc100-fe7c-11ea-8512-69f90cb65e48.gif"
+                            alt="Add the LinkedIn GIF Here"
+                          />
+                          <div class="card-body">
+                            <p class="card-text">Add the LinkedIn text here</p>
+                          </div>
+                        </div>
+                        <div class="card">
+                          <img
+                            class="card-img-top"
+                            src="https://user-images.githubusercontent.com/14011726/94132137-7d4fc100-fe7c-11ea-8512-69f90cb65e48.gif"
+                            alt="Add X GIF here"
+                          />
+                          <div class="card-body">
+                            <p class="card-text">X Text here</p>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div class="elementor-widget-container">
+                        <div class="elementor-button-wrapper container">
+                          <div className="redirect-buttons">
+                            {isNewUser ? (
+                              <>
+                                <button onClick={redirectToLinkedIn}>
+                                  Go to LinkedIn
+                                </button>
+
+                                <button onClick={redirectToX}>Go to X</button>
+                              </>
+                            ) : (
+                              <>
+                                <button onClick={aboutSocialScribe}>
+                                  About Us
+                                </button>
+                                <button onClick={openSettings}>
+                                  Open Settings
+                                </button>
+                              </>
+                            )}
+                          </div>
+                        </div>
+                      </div>
+                      <div class="spacing"></div>
+
+                      <a className="return-homepage" href="/">
+                        Return to Homepage
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="elementor-element elementor-element-f791ffd elementor-widget elementor-widget-spacer"
+                data-id="f791ffd"
                 data-element_type="widget"
-                data-widget_type="heading.default"
+                data-widget_type="spacer.default"
               >
                 <div class="elementor-widget-container">
-                  <h2 class="elementor-heading-title elementor-size-default">
-                    {heading}
-                  </h2>
-                  <p class="elementor-size-default">{para}</p>
-                  <div
-                    class="elementor-element elementor-element-9a3d259 elementor-widget__width-auto elementor-mobile-align-center elementor-widget-mobile__width-inherit elementor-align-left elementor-widget elementor-widget-button"
-                    data-id="9a3d259"
-                    data-element_type="widget"
-                    data-widget_type="button.default"
-                  >
-                    <div class="card-deck">
-                      <div class="card">
-                        <img
-                          class="card-img-top"
-                          src="https://user-images.githubusercontent.com/14011726/94132137-7d4fc100-fe7c-11ea-8512-69f90cb65e48.gif"
-                          alt="Add the LinkedIn GIF Here"
-                        />
-                        <div class="card-body">
-                          <p class="card-text">Add the LinkedIn text here</p>
-                        </div>
-                      </div>
-                      <div class="card">
-                        <img
-                          class="card-img-top"
-                          src="https://user-images.githubusercontent.com/14011726/94132137-7d4fc100-fe7c-11ea-8512-69f90cb65e48.gif"
-                          alt="Add X GIF here"
-                        />
-                        <div class="card-body">
-                          <p class="card-text">X Text here</p>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div class="elementor-widget-container">
-                      <div class="elementor-button-wrapper container">
-                        <div className="redirect-buttons">
-                          {isNewUser ? (
-                            <>
-                              <button onClick={redirectToLinkedIn}>
-                                Go to LinkedIn
-                              </button>
-
-                              <button onClick={redirectToX}>Go to X</button>
-                            </>
-                          ) : (
-                            <>
-                              <button onClick={aboutSocialScribe}>
-                                About Us
-                              </button>
-                              <button onClick={openSettings}>
-                                Open Settings
-                              </button>
-                            </>
-                          )}
-                        </div>
-                      </div>
-                    </div>
-                    <div class="spacing"></div>
-
-                    <a className="return-homepage" href="/">
-                      Return to Homepage
-                    </a>
+                  <div class="elementor-spacer">
+                    <div class="elementor-spacer-inner"></div>
                   </div>
                 </div>
               </div>
             </div>
-            <div
-              class="elementor-element elementor-element-f791ffd elementor-widget elementor-widget-spacer"
-              data-id="f791ffd"
-              data-element_type="widget"
-              data-widget_type="spacer.default"
-            >
-              <div class="elementor-widget-container">
-                <div class="elementor-spacer">
-                  <div class="elementor-spacer-inner"></div>
-                </div>
-              </div>
-            </div>
           </div>
+          <div
+            class="elementor-element elementor-element-3b42446 e-con-full e-flex e-con e-child"
+            data-id="3b42446"
+            data-element_type="container"
+          ></div>
         </div>
-        <div
-          class="elementor-element elementor-element-3b42446 e-con-full e-flex e-con e-child"
-          data-id="3b42446"
-          data-element_type="container"
-        ></div>
       </div>
-      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    </div>
+    </>
   );
 };
 

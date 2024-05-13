c79248497aba8cadb1737ebca4392fdea8273b85
aipoool <comdtvise@gmail.com>
2024-05-13 11:15:57 +0530

    Update RegisteredUser.js

--- src/components/js/RegisteredUser.js
+++ src/components/js/RegisteredUser.js
@@ -122,9 +122,9 @@ const RegisteredUser = ({ isNewUser = true }) => {
                   data-widget_type="heading.default"
                 >
                   <div class="elementor-widget-container">
-                    <h2 class="elementor-heading-title elementor-size-default">
+                    <h1 class="elementor-heading-title elementor-size-default">
                       {heading}
-                    </h2>
+                    </h1>
                     <p class="elementor-size-default">{para}</p>
                   </div>
                 </div>
@@ -237,7 +237,7 @@ const RegisteredUser = ({ isNewUser = true }) => {
               </>
             ) : (
               <>
-                <div class="row row-cols-1 row-cols-md-2 g-4">
+                {/* <div class="row row-cols-1 row-cols-md-2 g-4">
                   <div class="col">
                     <div class="card mb-3">
                       <img
@@ -275,45 +275,45 @@ const RegisteredUser = ({ isNewUser = true }) => {
                       </div>
                     </div>
                   </div>
-                </div>
-                {/* <div class="card-deck">
-                  <div class="card bg-dark text-white">
-                    <img
-                      class="card-img"
-                      src="https://i0.wp.com/www.galvanizeaction.org/wp-content/uploads/2022/06/Wow-gif.gif?fit=450%2C250&ssl=1"
-                      alt="Card image"
-                    />
-                    <div class="card-img-overlay">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">
-                        This is a wider card with supporting text below as a
-                        natural lead-in to additional content. This content is a
-                        little bit longer.
-                      </p>
-                      <button type="button" class="btn btn-primary">
-                        Primary
-                      </button>
-                    </div>
-                  </div>
-                  <div class="card bg-dark text-white">
-                    <img
-                      class="card-img"
-                      src="https://i0.wp.com/www.galvanizeaction.org/wp-content/uploads/2022/06/Wow-gif.gif?fit=450%2C250&ssl=1"
-                      alt="Card image"
-                    />
-                    <div class="card-img-overlay">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">
-                        This is a wider card with supporting text below as a
-                        natural lead-in to additional content. This content is a
-                        little bit longer.
-                      </p>
-                      <button type="button" class="btn btn-primary">
-                        Primary
-                      </button>
-                    </div>
-                  </div>
                 </div> */}
+                <div class="card-deck">
+                  <div class="card bg-dark text-white">
+                    <img
+                      class="card-img"
+                      src="https://i0.wp.com/www.galvanizeaction.org/wp-content/uploads/2022/06/Wow-gif.gif?fit=450%2C250&ssl=1"
+                      alt="Card image"
+                    />
+                    <div class="card-img-overlay">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">
+                        This is a wider card with supporting text below as a
+                        natural lead-in to additional content. This content is a
+                        little bit longer.
+                      </p>
+                      <button type="button" class="btn btn-primary">
+                        Primary
+                      </button>
+                    </div>
+                  </div>
+                  <div class="card bg-dark text-white">
+                    <img
+                      class="card-img"
+                      src="https://i0.wp.com/www.galvanizeaction.org/wp-content/uploads/2022/06/Wow-gif.gif?fit=450%2C250&ssl=1"
+                      alt="Card image"
+                    />
+                    <div class="card-img-overlay">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">
+                        This is a wider card with supporting text below as a
+                        natural lead-in to additional content. This content is a
+                        little bit longer.
+                      </p>
+                      <button type="button" class="btn btn-primary">
+                        Primary
+                      </button>
+                    </div>
+                  </div>
+                </div>
               </>
             )}
           </div>

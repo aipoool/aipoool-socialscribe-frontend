c21b9c9590fcdc61abdaeacbedfaad69c4f6672c
aipoool <comdtvise@gmail.com>
2024-05-15 20:13:12 +0530

    Update OpenAIKeyForm.js

--- src/components/js/OpenAIKeyForm.js
+++ src/components/js/OpenAIKeyForm.js
@@ -102,105 +102,69 @@ const OpenAIKeyForm = ({ userId }) => {
                       data-element_type="widget"
                       data-widget_type="button.default"
                     >
-                      <div class="elementor-widget-container">
-                        <div class="elementor-button-wrapper container">
-                          {/* <button class="elementor-button elementor-button-link elementor-size-sm"  target="_blank">
-                                          <span class="elementor-button-content-wrapper">
-                                              <span class="elementor-button-icon elementor-align-icon-left">
-                                                  <svg aria-hidden="true" class="e-font-icon-svg e-fab-chrome" viewBox="0 0 496 512" xmlns="http://www.w3.org/2000/svg">
-                                                      <path d="M131.5 217.5L55.1 100.1c47.6-59.2 119-91.8 192-92.1 42.3-.3 85.5 10.5 124.8 33.2 43.4 25.2 76.4 61.4 97.4 103L264 133.4c-58.1-3.4-113.4 29.3-132.5 84.1zm32.9 38.5c0 46.2 37.4 83.6 83.6 83.6s83.6-37.4 83.6-83.6-37.4-83.6-83.6-83.6-83.6 37.3-83.6 83.6zm314.9-89.2L339.6 174c37.9 44.3 38.5 108.2 6.6 157.2L234.1 503.6c46.5 2.5 94.4-7.7 137.8-32.9 107.4-62 150.9-192 107.4-303.9zM133.7 303.6L40.4 120.1C14.9 159.1 0 205.9 0 256c0 124 90.8 226.7 209.5 244.9l63.7-124.8c-57.6 10.8-113.2-20.8-139.5-72.5z"></path>
-                                                  </svg> 
-                                              </span>
-                                              <span class="elementor-button-text">Sign in With Google</span>
-                                          </span>
-                                      </button> */}
-                          {/* <div class="card">
-                            <form onSubmit={handleSubmit}>
-                              <input
-                                id="openai-key-input"
-                                type="text"
-                                value={openAIKey}
-                                onChange={(e) => {
-                                  setOpenAIKey(e.target.value);
-                                }}
-                                placeholder="Enter your OpenAI Key here"
-                                required
+                      <div class="container snipcss-EmI9c">
+                        <div class="card login-card">
+                          <div class="row no-gutters">
+                            <div class="col-md-5">
+                              <img
+                                src={openAIKeyEnter}
+                                alt="login"
+                                class="login-card-img"
                               />
-                              <button
-                                className="buttonSubmit"
-                                type="submit"
-                                disabled={isSubmitting}
-                              >
-                                {isSubmitting ? "Submitting..." : "Submit"}
-                              </button>
-                            </form>
-                            {message && <p>{message}</p>}
-                          </div> */}
-
-                          <div class="container snipcss-EmI9c">
-                            <div class="card login-card">
-                              <div class="row no-gutters">
-                                <div class="col-md-5">
+                            </div>
+                            <div class="col-md-7">
+                              <div class="card-body">
+                                <div class="brand-wrapper">
                                   <img
-                                    src={openAIKeyEnter}
-                                    alt="login"
-                                    class="login-card-img"
+                                    src={logoImage}
+                                    alt="logo"
+                                    class="logo"
                                   />
                                 </div>
-                                <div class="col-md-7">
-                                  <div class="card-body">
-                                    <div class="brand-wrapper">
-                                      <img
-                                        src={logoImage}
-                                        alt="logo"
-                                        class="logo"
-                                      />
-                                    </div>
-                                    <p class="login-card-description">
-                                      Please enter your OpenAI Key{" "}
-                                    </p>
-                                    <form action="#!">
-                                      <div class="form-group">
-                                        <label for="email" class="sr-only">
-                                          Email
-                                        </label>
-                                        <input
-                                          type="email"
-                                          name="email"
-                                          id="email"
-                                          class="form-control"
-                                          placeholder="e.g. sk-proj-12345..."
-                                        />
-                                      </div>
-
-                                      <input
-                                        name="login"
-                                        id="login"
-                                        class="btn btn-block login-btn mb-4"
-                                        type="button"
-                                        value="Submit"
-                                      />
-                                    </form>
-                                    <a href="#!" class="forgot-password-link">
-                                      Return to Homepage
-                                    </a>
-                                    <p class="login-card-footer-text">
-                                      How to generate OpenAI Key?{" "}
-                                      <a href="#!" class="text-reset">
-                                        Click here
-                                      </a>
-                                    </p>
-                                    <nav class="login-card-footer-nav">
-                                      <a href="#!">Terms of use.</a>
-                                      <a href="#!">Privacy policy</a>
-                                    </nav>
+                                <p class="login-card-description">
+                                  Please enter your OpenAI Key{" "}
+                                </p>
+                                <form action="#!">
+                                  <div class="form-group">
+                                    <label for="email" class="sr-only">
+                                      Email
+                                    </label>
+                                    <input
+                                      type="email"
+                                      name="email"
+                                      id="email"
+                                      class="form-control"
+                                      placeholder="e.g. sk-proj-12345..."
+                                    />
                                   </div>
-                                </div>
+
+                                  <input
+                                    name="login"
+                                    id="login"
+                                    class="btn btn-block login-btn mb-4"
+                                    type="button"
+                                    value="Submit"
+                                  />
+                                </form>
+                                <a href="#!" class="forgot-password-link">
+                                  Return to Homepage
+                                </a>
+                                <p class="login-card-footer-text">
+                                  How to generate OpenAI Key?{" "}
+                                  <a href="#!" class="text-reset">
+                                    Click here
+                                  </a>
+                                </p>
+                                <nav class="login-card-footer-nav">
+                                  <a href="#!">Terms of use.</a>
+                                  <a href="#!">Privacy policy</a>
+                                </nav>
                               </div>
                             </div>
                           </div>
                         </div>
                       </div>
+
                       <div class="spacing"></div>
                       <a
                         className="return-homepage"

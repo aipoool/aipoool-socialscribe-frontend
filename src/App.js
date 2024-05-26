9ba40b8d24e8cd58d2cc5bdedd38b88e399d29a4
aipoool <comdtvise@gmail.com>
2024-05-26 12:14:35 +0530

    chore: Added the payment method

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import './App.css';
-import Headers from './components/js/Header';
-import Login from './components/js/Login';
-import EnterOpenAIKey from './components/js/EnterOpenAIKey';
-import RegisteredUser from './components/js/RegisteredUser'; 
-import { Routes, Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import Headers from "./components/js/Header";
+import PayLanding from "./components/js/PaymentLanding";
+import Success from "./components/js/success";
+import Cancel from "./components/js/Cancel";
+import Login from "./components/js/Login";
+import EnterOpenAIKey from "./components/js/EnterOpenAIKey";
+import RegisteredUser from "./components/js/RegisteredUser";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <>
       <Headers />
-        <Routes>
-          <Route path='/' element = {<Login />} />
-          <Route path='/login' element = {<Login />} />
-          <Route path='/enter-your-key' element={<EnterOpenAIKey />} />
-          <Route path='/registered' element={<RegisteredUser />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/enter-your-key" element={<EnterOpenAIKey />} />
+        <Route path="/registered" element={<RegisteredUser />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/cancel" element={<Cancel />} />
+        <Route path="/choose-your-plan" element={<PayLanding />} />
+      </Routes>
     </>
   );
 }
---
+++ src/components/js/PaymentLanding.js
@@ -0,0 +1,277 @@
+import React from "react";
+import "../css/landing.css";
+import axios from "axios";
+import { loadStripe } from "@stripe/stripe-js";
+
+const paymentlanding = () => {
+  /** GET THE USER DATA FROM THE SOURCE AND ADD EMAIL TO THE BODY TO HARDCODE IT IN THE BLANK */
+  async function makePayment(planType) {
+    const stripe = await loadStripe(
+      "pk_test_51NkpdNSGYG2CnOjscEUK9B0JfqNSzZEqj1GXLZEHPJrzCGPfTDv2DnleEid0HVNvmlf0qUVjNFTRV3UzpAsucioU00Kthd5o4Z"
+    );
+    let planPay, price;
+    if (planType === "free") {
+      console.log("Processing free plan");
+      // Add your logic for processing free plan
+    } else if (planType === "premium") {
+      console.log("Processing premium plan");
+      planPay = "SocialScribe Premium Plan";
+      price = 1;
+    } else if (planType === "pro") {
+      console.log("Processing pro plan");
+
+      planPay = "SocialScribe Pro Plan";
+      price = 2;
+    } else {
+      console.log("Invalid plan");
+    }
+
+    // Call the userdata and send the email and id via the body
+    const response = await axios.post(
+      "http://localhost:1997/api/create-checkout-session",
+      {
+        data: {
+          plan: planPay,
+          type: planType,
+          price: price,
+          userEmail: "frosticpanda9ae23@gmail.com",
+          userId: "664e0d668d57494e9cd43559",
+        },
+      },
+      { withCredentials: true }
+    );
+
+    console.log(response);
+    console.log(response.data.redirectUrl);
+    console.log(response.config.data);
+
+    if (response.data && response.data.redirectUrl) {
+      window.location.href = response.data.redirectUrl; // rediret the user to billing portal if already subscribed
+    } else {
+      const session = response.data;
+      console.log(session);
+
+      const result = stripe.redirectToCheckout({
+        sessionId: session.id,
+      });
+
+      if (result.error) {
+        console.log(result.error);
+      }
+    }
+  }
+
+  return (
+    <>
+      <div
+        class="elementor-element elementor-element-9e0036e e-flex e-con-boxed e-con e-parent e-lazyloaded snipcss-97bdC"
+        data-id="9e0036e"
+        data-element_type="container"
+        data-settings='{"background_background":"classic"}'
+      >
+        <div class="e-con-inner">
+          <div
+            class="elementor-element elementor-element-ed79abc e-con-full e-flex e-con e-child"
+            data-id="ed79abc"
+            data-element_type="container"
+          >
+            <div
+              class="elementor-element elementor-element-597b965 e-con-full e-flex e-con e-child"
+              data-id="597b965"
+              data-element_type="container"
+            >
+              <div
+                class="elementor-element elementor-element-11d9bb4 e-con-full e-flex e-con e-child"
+                data-id="11d9bb4"
+                data-element_type="container"
+              >
+                <div
+                  class="elementor-element elementor-element-1f4402a elementor-widget__width-initial elementor-widget-tablet__width-initial elementor-widget elementor-widget-heading"
+                  data-id="1f4402a"
+                  data-element_type="widget"
+                  data-widget_type="heading.default"
+                >
+                  <div class="elementor-widget-container">
+                    <h1 class="elementor-heading-title elementor-size-default">
+                      Welcome to SocialScribe!
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="spacing"></div>
+
+            <div class="container snipcss-iaZTI">
+              <h5 class="text-center pricing-table-subtitle">PRICING PLAN</h5>
+              <h1 class="text-center pricing-table-title">Pricing Table</h1>
+              <div class="row">
+                <div class="col-md-4">
+                  <div class="card pricing-card pricing-plan-basic">
+                    <div class="card-body">
+                      <i class="mdi mdi-cube-outline pricing-plan-icon"></i>
+                      <p class="pricing-plan-title">Basic</p>
+                      <h3 class="pricing-plan-cost ml-auto">FREE</h3>
+                      <ul class="pricing-plan-features">
+                        <li>Unlimited conferences</li>
+                        <li>100 participants max</li>
+                        <li>Custom Hold Music</li>
+                        <li>10 participants max</li>
+                      </ul>
+                      <button
+                        class="btn pricing-plan-purchase-btn"
+                        onClick={() => makePayment("free")}
+                      >
+                        Free
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <div class="card pricing-card pricing-card-highlighted  pricing-plan-pro">
+                    <div class="card-body">
+                      <i class="mdi mdi-trophy pricing-plan-icon"></i>
+                      <p class="pricing-plan-title">Pro</p>
+                      <h3 class="pricing-plan-cost ml-auto">$49</h3>
+                      <ul class="pricing-plan-features">
+                        <li>Unlimited conferences</li>
+                        <li>100 participants max</li>
+                        <li>Custom Hold Music</li>
+                        <li>10 participants max</li>
+                      </ul>
+                      <button
+                        class="btn pricing-plan-purchase-btn"
+                        onClick={() => makePayment("premium")}
+                      >
+                        Premium
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <div class="card pricing-card pricing-plan-enterprise">
+                    <div class="card-body">
+                      <i class="mdi mdi-wallet-giftcard pricing-plan-icon"></i>
+                      <p class="pricing-plan-title">Enterprise</p>
+                      <h3 class="pricing-plan-cost ml-auto">$69</h3>
+                      <ul class="pricing-plan-features">
+                        <li>Unlimited conferences</li>
+                        <li>100 participants max</li>
+                        <li>Custom Hold Music</li>
+                        <li>10 participants max</li>
+                      </ul>
+                      <button
+                        class="btn pricing-plan-purchase-btn"
+                        onClick={() => makePayment("pro")}
+                      >
+                        Pro
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="elementor-element elementor-element-f791ffd elementor-widget elementor-widget-spacer"
+            data-id="f791ffd"
+            data-element_type="widget"
+            data-widget_type="spacer.default"
+          >
+            <div class="elementor-widget-container">
+              <div class="elementor-spacer">
+                <div class="elementor-spacer-inner"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        class="site-below-footer-wrap ast-builder-grid-row-container site-footer-focus-item ast-builder-grid-row-3-cheavy ast-builder-grid-row-tablet-3-equal ast-builder-grid-row-mobile-full ast-footer-row-stack ast-footer-row-tablet-stack ast-footer-row-mobile-stack snipcss-YE11r"
+        data-section="section-below-footer-builder"
+      >
+        <div class="ast-builder-grid-row-container-inner">
+          <div class="ast-builder-footer-grid-columns site-below-footer-inner-wrap ast-builder-grid-row">
+            <div class="site-footer-below-section-1 site-footer-section site-footer-section-1">
+              <div
+                class="footer-widget-area widget-area site-footer-focus-item ast-footer-html-1"
+                data-section="section-fb-html-1"
+              >
+                <div class="ast-header-html inner-link-style-">
+                  <div class="ast-builder-html-element">
+                    <p>
+                      <img
+                        class="size-full wp-image-1628"
+                        src="https://socialscribe.aipoool.com/wp-content/uploads/2024/05/AIpool-1.png"
+                        alt="footer logo"
+                        width="250"
+                        height="100"
+                      />
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="site-footer-below-section-2 site-footer-section site-footer-section-2">
+              <div
+                class="ast-builder-layout-element ast-flex site-footer-focus-item ast-footer-copyright"
+                data-section="section-footer-builder"
+              >
+                <div class="ast-footer-copyright">
+                  <p>© 2024 socialscribe. Powered by [aipoool].</p>
+                </div>
+              </div>
+            </div>
+            <div class="site-footer-below-section-3 site-footer-section site-footer-section-3">
+              <div
+                class="footer-widget-area widget-area site-footer-focus-item"
+                data-section="section-footer-menu"
+              >
+                <div class="footer-bar-navigation">
+                  <nav
+                    class="site-navigation ast-flex-grow-1 navigation-accessibility footer-navigation"
+                    id="footer-site-navigation"
+                    aria-label="Site Navigation: Footer - terms"
+                    itemtype="https://schema.org/SiteNavigationElement"
+                    itemscope="itemscope"
+                  >
+                    <div class="footer-nav-wrap">
+                      <ul
+                        id="astra-footer-menu"
+                        class="ast-nav-menu ast-flex astra-footer-horizontal-menu astra-footer-tablet-vertical-menu astra-footer-mobile-vertical-menu"
+                      >
+                        <li
+                          id="menu-item-808"
+                          class="menu-item menu-item-type-custom menu-item-object-custom menu-item-808"
+                        >
+                          <a
+                            href="https://aipoool.com/terms-conditions/"
+                            class="menu-link"
+                          >
+                            Terms and Conditions
+                          </a>
+                        </li>
+                        <li
+                          id="menu-item-809"
+                          class="menu-item menu-item-type-custom menu-item-object-custom menu-item-809"
+                        >
+                          <a
+                            href="https://aipoool.com/privacy-policy/"
+                            class="menu-link"
+                          >
+                            Privacy Policy
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </nav>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default paymentlanding;

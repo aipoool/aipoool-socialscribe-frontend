e32f21c7a58b7f4d380cad50f2f273facded3cf9
aipoool <comdtvise@gmail.com>
2024-05-31 21:59:45 +0530

    chore: Resetting - 2

--- src/components/js/PaymentLanding.js
+++ src/components/js/PaymentLanding.js
@@ -13,20 +13,23 @@ const PaymentLanding = () => {
         "https://aipoool-socialscribe-backend.onrender.com/get-user-data",
         { withCredentials: true }
       );
-      if (response.status === 200 || response.status === 302) { 
-        console.log(response.data.user); 
-        console.log(`${response.data.user.email} :::::: ${response.data.user._id}`); 
+      if (response.status === 200 || response.status === 302) {
+        console.log(response.data.user);
+        console.log(
+          `${response.data.user.email} :::::: ${response.data.user._id}`
+        );
         setUserdata(response.data.user);
       }
     } catch (error) {
       if (error.response && error.response.status === 401) {
-        window.location.href = 'https://socialscribe-aipoool.onrender.com/login';
+        window.location.href =
+          "https://socialscribe-aipoool.onrender.com/login";
       } else {
         console.log("error", error);
       }
     }
   };
-  
+
   useEffect(() => {
     fetchData();
   }, []);
@@ -36,24 +39,21 @@ const PaymentLanding = () => {
       "pk_test_51NkpdNSGYG2CnOjscEUK9B0JfqNSzZEqj1GXLZEHPJrzCGPfTDv2DnleEid0HVNvmlf0qUVjNFTRV3UzpAsucioU00Kthd5o4Z"
     );
 
-
-
     let sPriceId, sProductId;
     if (planType === "free") {
       console.log("Processing free plan");
       // Add your logic for processing free plan
     } else if (planType === "premium") {
-
       console.log("Processing premium plan");
-      sPriceId = 'price_1PKzSsSGYG2CnOjsDpM6cUau';
-      sProductId = 'prod_QBMBmxVrYZ26Hp';
+      sPriceId = "price_1PKzSsSGYG2CnOjsDpM6cUau";
+      sProductId = "prod_QBMBmxVrYZ26Hp";
 
       // planPay = "SocialScribe Premium Plan";
       // price = 1;
     } else if (planType === "pro") {
       console.log("Processing pro plan");
-      sPriceId = 'price_1PKzTSSGYG2CnOjs0iTitDDU';
-      sProductId = 'prod_QBMBqKnsx3xsxg';
+      sPriceId = "price_1PKzTSSGYG2CnOjs0iTitDDU";
+      sProductId = "prod_QBMBqKnsx3xsxg";
     } else {
       console.log("Invalid plan");
     }
@@ -72,7 +72,6 @@ const PaymentLanding = () => {
       },
       { withCredentials: true }
     );
-
 
     if (response.data && response.data.redirectUrl) {
       window.location.href = response.data.redirectUrl; // rediret the user to billing portal if already subscribed
@@ -93,80 +92,12 @@ const PaymentLanding = () => {
   return (
     <>
       <div
-        class="elementor-element elementor-element-9e0036e e-flex e-con-boxed e-con e-parent e-lazyloaded snipcss-97bdC"
+        class="elementor-element elementor-element-9e0036e e-flex e-con-boxed e-con e-parent e-lazyloaded snipcss-M42OB"
         data-id="9e0036e"
         data-element_type="container"
         data-settings='{"background_background":"classic"}'
       >
-        <div class="containercard snipcss-iaZTI">
-          <h5 class="text-center pricing-table-subtitle">PRICING PLAN</h5>
-          <h1 class="text-center pricing-table-title">Pricing Table</h1>
-          <div class="row">
-            <div class="col-md-4">
-              <div class="card pricing-card pricing-plan-basic">
-                <div class="card-body">
-                  <i class="mdi mdi-cube-outline pricing-plan-icon"></i>
-                  <p class="pricing-plan-title">Basic</p>
-                  <h3 class="pricing-plan-cost ml-auto">FREE</h3>
-                  <ul class="pricing-plan-features">
-                    <li>Unlimited conferences</li>
-                    <li>100 participants max</li>
-                    <li>Custom Hold Music</li>
-                    <li>10 participants max</li>
-                  </ul>
-                  <button
-                    class="btn pricing-plan-purchase-btn"
-                    onClick={() => makePayment("free")}
-                  >
-                    Free
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card pricing-card pricing-card-highlighted  pricing-plan-pro">
-                <div class="card-body">
-                  <i class="mdi mdi-trophy pricing-plan-icon"></i>
-                  <p class="pricing-plan-title">Pro</p>
-                  <h3 class="pricing-plan-cost ml-auto">$49</h3>
-                  <ul class="pricing-plan-features">
-                    <li>Unlimited conferences</li>
-                    <li>100 participants max</li>
-                    <li>Custom Hold Music</li>
-                    <li>10 participants max</li>
-                  </ul>
-                  <button
-                    class="btn pricing-plan-purchase-btn"
-                    onClick={() => makePayment("premium")}
-                  >
-                    Premium
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card pricing-card pricing-plan-enterprise">
-                <div class="card-body">
-                  <i class="mdi mdi-wallet-giftcard pricing-plan-icon"></i>
-                  <p class="pricing-plan-title">Enterprise</p>
-                  <h3 class="pricing-plan-cost ml-auto">$69</h3>
-                  <ul class="pricing-plan-features">
-                    <li>Unlimited conferences</li>
-                    <li>100 participants max</li>
-                    <li>Custom Hold Music</li>
-                    <li>10 participants max</li>
-                  </ul>
-                  <button
-                    class="btn pricing-plan-purchase-btn"
-                    onClick={() => makePayment("pro")}
-                  >
-                    Pro
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <div class="e-con-inner"></div>
       </div>
       <div className="footer">
         <div

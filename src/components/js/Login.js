3d57b0538e5420d1dcdad33c68c3358a51f925e0
aipoool <comdtvise@gmail.com>
2024-08-23 22:39:42 +0530

    Update Login.js

--- src/components/js/Login.js
+++ src/components/js/Login.js
@@ -5,10 +5,12 @@ import {Cloudinary} from "@cloudinary/url-gen";
 import {AdvancedImage} from '@cloudinary/react';
 const Login = () => {
   const loginwithgoogle = () => {
-    window.open(
-      "https://socialscribe-v1-backend.onrender.com/auth/google/callback",
-      "_self"
-    );
+    // window.open(
+    //   "https://socialscribe-v1-backend.onrender.com/auth/google/callback",
+    //   "_self"
+    // );
+    window.location.href = "https://socialscribe-v1-backend.onrender.com/auth/google/url";
+
   };
 
   const cloud = new Cloudinary({
